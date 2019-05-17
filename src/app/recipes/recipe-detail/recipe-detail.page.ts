0870bebf25a7518870e49f1882d9caabbd74022e
niketl16 <niketl16@gmail.com>
2019-05-17 18:15:34 +0530

    Dependency injection (Erroed)

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,25 +11,42 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailPage implements OnInit {
 
-loadedRecipe: Recipe;
+  loadedRecipe: Recipe;
 
   constructor(private activatedRoute: ActivatedRoute,
-     private recipeService:RecipesService,
-     private router: Router) { }
+    private recipeService: RecipesService,
+    private router: Router,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('recipeId')){
+      if (!paramMap.has('recipeId')) {
         //redirect 
         return;
       }
       const recipeId = paramMap.get('recipeId');
-      this.loadedRecipe=this.recipeService.getRecipe(recipeId);
+      this.loadedRecipe = this.recipeService.getRecipe(recipeId);
     });
   }
 
-  onDeleteRecipe(){
-    this.recipeService.deleteRecipe(this.loadedRecipe.id);
-    this.router.navigate(['/recipes']);  
+  onDeleteRecipe() {
+    this.alertCtrl.create({
+      header: "Are u sure?", message: "really delete ?",
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel'
+      },
+      {
+        text: 'Delete',
+        handler: () => {
+          this.recipeService.deleteRecipe(this.loadedRecipe.id);
+          this.router.navigate(['./recipes']);
+        }
+      }]
+    })
+    .then(alertEl => {
+      alertEl.present();
+    });
+    
   }
 }
